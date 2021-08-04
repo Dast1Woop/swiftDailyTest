4293725626bccd48bd58e3b2fde97096b30b6d49
myl <myldast1@126.com>
2021-08-04 16:35:41 +0800

    update url

--- 高德/AMap_iOS_Demo/MAMapKit_3D_Demo/MAMapKit-3D-Demo-swift/samples/overlay/TileOverlayViewController.swift
+++ 高德/AMap_iOS_Demo/MAMapKit_3D_Demo/MAMapKit-3D-Demo-swift/samples/overlay/TileOverlayViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-let kURL4TileOverlayLevel0 = "https://dev.indoormap.huatugz.com/xyztiles/a11y/{z}/{x}/{y}.png?tileSize=512&scale=2"
+
+let kURL4TileOverlayLevel0 = "https://dev.indoormap.huatugz.com/xyztiles/wheelchairtile/14/13384/7137.png?tileSize=512&scale=2"
 
 class TileOverlayViewController: UIViewController,  MAMapViewDelegate {
     
