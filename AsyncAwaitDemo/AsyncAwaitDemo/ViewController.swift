5a404f9aaa510b32ec00317641e4506fcfa2b981
myl <myldast1@126.com>
2021-09-14 14:56:04 +0800

    swift package test

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AsyncAwaitDemo
+//
+//  Created by LongMa on 2021/9/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        test()
+    }
+
+    func test() {
+        
+    }
+    
+    func needTimeTask() async -> (String,Error){
+        
+    }
+
+}
+
