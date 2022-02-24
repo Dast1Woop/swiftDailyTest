1a3d6a49c8642e58a14762c435fbcc2871cb6ac5
myl <myldast1@126.com>
2022-02-24 23:23:40 +0800

    private 属性：只对类有限制，类内对象，可以访问。实例就是累加器的实现。

---
+++ PrivateTest/PrivateTest/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  PrivateTest
+//
+//  Created by LongMa on 2022/2/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let num1 = Number.init(shibu: 1, xubu: 2)
+let num2 = Number.init(shibu: 10, xubu: 20)
+num1.add(another: num2)
+
