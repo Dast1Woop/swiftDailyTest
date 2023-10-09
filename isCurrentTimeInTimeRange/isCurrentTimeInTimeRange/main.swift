75813fcfe3ce7984eca2e6f68f90f275da7925bc
myl <myldast1@126.com>
2023-10-09 17:09:28 +0800

    new

--- isCurrentTimeInTimeRange/isCurrentTimeInTimeRange/main.swift
+++ isCurrentTimeInTimeRange/isCurrentTimeInTimeRange/main.swift
@@ -7,7 +7,7 @@ if Date.isCurrentTimeIn(timeRanges: ["6:00":"12:00", "13:00":"18:00"]) {
     print("1 不在时间范围内")
 }
 
-if Date.isCurrentTimeIn(timeRanges: ["6:00":"16:39", "16:40":"16:45"]) {
+if Date.isCurrentTimeIn(timeRanges: ["6:00":"16:39", "16:40":"00:00"]) {
     print("2 在时间范围内")
 }else {
     print("2 不在时间范围内")
